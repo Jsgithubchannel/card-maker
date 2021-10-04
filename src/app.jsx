0816505e5cbc75d5a2d1278cc0e7cc65f64c609e
Jsgithubchannel <jsschannel99@gmail.com>
2021-10-04 23:17:38 +0900

    add login service

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,8 @@
 import "./app.module.css";
+import Login from "./components/login/Login";
 
-function App() {
-  return <h1>Hello </h1>;
+function App({ authService }) {
+  return <Login authService={authService} />;
 }
 
 export default App;
