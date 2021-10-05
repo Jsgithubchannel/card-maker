128dbc006b39e17025ccc8341e585eb0d2fefc88
Jsgithubchannel <jsschannel99@gmail.com>
2021-10-05 16:29:57 +0900

    add card and eit form component

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Editor.module.css";
+import CardEditForm from "./../card_edit_form/CardEditForm";
 
-const Editor = () => {
+const Editor = ({ cards }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
+      {cards.map((card) => (
+        <CardEditForm card={card} />
+      ))}
     </section>
   );
 };
--- src/components/maker/Maker.jsx
+++ src/components/maker/Maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../footer/Footer";
 import Header from "./../header/Header";
 import styles from "./Maker.module.css";
@@ -7,6 +7,41 @@ import Editor from "../editor/Editor";
 import Preview from "../preview/Preview";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "JS1",
+      company: "Samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "js@gmail.com",
+      message: "go for it",
+      fileName: "js",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "JS2",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "js@gmail.com",
+      message: "go for it",
+      fileName: "js",
+      fileURL: "js.png",
+    },
+    {
+      id: "3",
+      name: "JS3",
+      company: "Samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "js@gmail.com",
+      message: "go for it",
+      fileName: "js",
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -23,8 +58,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
